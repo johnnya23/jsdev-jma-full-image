132ee2dc6b0f426bb009188c0ffed1a9ba898fc2
johnnya23 <johnantonacci23@gmail.com>
2018-11-05 09:49:42 -0700

    remove dragging

--- jma-big-slider.js
+++ jma-big-slider.js
@@ -1,36 +1,5 @@
 jQuery(document).ready(function($) {
 
-    //http://stackoverflow.com/questions/2424191/how-to-make-a-element-draggable-use-jquery
-    function handle_dragging(e) {
-        //var left = my_dragging.offset0.left + (e.pageX - my_dragging.pageX0);
-        var top = my_dragging.offset0.top + (e.pageY - my_dragging.pageY0);
-        $(my_dragging.elem)
-            .offset({
-                top: top /*, left: left*/
-            });
-    }
-
-    function handle_mouseup(e) {
-        $('body')
-            .off('mousemove', handle_dragging)
-            .off('mouseup', handle_mouseup);
-    }
-
-    function handle_mousedown(e) {
-        if ($('body').is('.big_slider') && $(window).width() > 768) {
-            window.my_dragging = {};
-            //my_dragging.pageX0 = e.pageX;
-            my_dragging.pageY0 = e.pageY;
-            my_dragging.elem = this;
-            my_dragging.offset0 = $(this).offset();
-            $('body')
-                .on('mouseup', handle_mouseup)
-                .on('mousemove', handle_dragging);
-        }
-    }
-    //$('.site-main').mousedown(handle_mousedown);
-
-
     $('.site-main').on('click', '#menu-home > li > a', function(event) {
 
         event.preventDefault();
